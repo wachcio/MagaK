359de2162b0993c07d887d85d8a1c1056c579958
wachcio <wachcio@poczta.fm>
2021-07-03 01:15:37 +0200

    divide 3

---
+++ Tydzień 1/liczbyPrzezTrzy.js
@@ -0,0 +1,8 @@
+const countStart = 1;
+const countEnd = 100;
+
+for (let i = countStart; i <= countEnd; i++) {
+    if (i % 3 === 0) {
+        console.log(`Liczba ${i} jest podzielna przez 3`);
+    }
+}

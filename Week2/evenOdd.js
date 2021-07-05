902acb4ebc6f5a5765db04c2d22c9714622849d2
wachcio <wachcio@poczta.fm>
2021-07-06 00:58:19 +0200

    evenOdd finish

--- Week2/evenOdd.js
+++ Week2/evenOdd.js
@@ -1,9 +1,23 @@
+// const even = function (number) {}
+const even = (number) => number;
+// const odd = function (number) {}
+const odd = (number) => number * 2;
+
 // const calculator = function(number) {
-const evenOdd = (number) => {
-    if (number === undefined || Number.isNaN(Number(number)) === true)
+const checkNumber = (number) => {
+    if (
+        number === undefined ||
+        Number.isNaN(Number(number) || number == null) === true
+    )
         return console.log('Wrong input data');
+
+    if (Number(number) % 2 == 0) {
+        console.log(even(number));
+    } else {
+        console.log(odd(number));
+    }
 };
 
-const numberOne = prompt('Podaj liczbę');
+const number = prompt('Podaj liczbę');
 
-calculator(numberOne, numberTwo);
+checkNumber(number);

192be0bb730198bf2a40dffbac60726598364b2e
wachcio <wachcio@poczta.fm>
2021-07-06 00:46:53 +0200

    evenOdd basic

---
+++ Week2/evenOdd.js
@@ -0,0 +1,9 @@
+// const calculator = function(number) {
+const evenOdd = (number) => {
+    if (number === undefined || Number.isNaN(Number(number)) === true)
+        return console.log('Wrong input data');
+};
+
+const numberOne = prompt('Podaj liczbę');
+
+calculator(numberOne, numberTwo);

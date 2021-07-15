a8b6357ddc99492bc2e34207493465153cf49186
wachcio <wachcio@poczta.fm>
2021-07-15 23:40:02 +0200

    refactor

--- Week3/DOM/js/app.js
+++ Week3/DOM/js/app.js
@@ -1,17 +1,15 @@
 const counterH1 = document.querySelector('.counter');
 const btnInc = document.querySelector('.btn-counter-inc');
 const btnDec = document.querySelector('.btn-counter-dec');
-const body = document.body;
 let counter = 0;
 
 const changeCounter = (operation = '+') => {
     if (operation === '+') {
         counter++;
-        counterH1.innerHTML = counter;
     } else {
         counter--;
-        counterH1.innerHTML = counter;
     }
+    counterH1.innerHTML = counter;
 };
 
 btnInc.addEventListener('click', () => {

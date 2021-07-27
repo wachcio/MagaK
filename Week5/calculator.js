80bb3cd4ea1f36273b595dfc5dd67991492bd741
wachcio <wachcio@poczta.fm>
2021-07-27 23:56:50 +0200

    testProvideNumber and trim

--- Week5/calculator.js
+++ Week5/calculator.js
@@ -34,14 +34,24 @@ ${this.multiply()}`;
         }
     }
 
+    testProvideNumber(nr) {
+        if (Number.isNaN(Number(nr)) || nr == null || nr == '') {
+            if (nr === this.a) {
+                throw new Error('Number 1 is not valid');
+            } else {
+                throw new Error('Number 2 is not valid');
+            }
+        }
+        this.a = Number(this.a);
+        this.b = Number(this.b);
+    }
+
     provideNumbers() {
         try {
-            this.a = Number(prompt(`Provide number 1 to calculate`));
-            if (Number.isNaN(Number(this.a)) || this.a == null || this.a == '')
-                throw new Error('Number 1 is not valid');
-            this.b = Number(prompt(`Provide number 2 to calculate`));
-            if (Number.isNaN(Number(this.b)) || this.a == null || this.b == '')
-                throw new Error('Number 2 is not valid');
+            this.a = prompt(`Provide number 1 to calculate`).trim();
+            this.testProvideNumber(this.a);
+            this.b = prompt(`Provide number 2 to calculate`).trim();
+            this.testProvideNumber(this.b);
 
             console.log(this.calculate());
         } catch (err) {

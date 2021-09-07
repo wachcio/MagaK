761cfa3dd681910e4d1d7ed43a2d4f14d441b52a
wachcio <wachcio@poczta.fm>
2021-09-07 13:35:56 +0200

    decrypt.js checksum working

--- Level2/Week4/Day1/Encrypt/decrypt.js
+++ Level2/Week4/Day1/Encrypt/decrypt.js
@@ -1,27 +1,32 @@
 require('dotenv').config(); // import variables from .env file. .env file not send to Github
 const fs = require('fs').promises;
+const { createHmac } = require('crypto');
 const arg = require('../../../../helpers/arguments');
 const { decryptText } = require('../../../../helpers/cipher');
 
 (async () => {
-    if (!arg.validate(['path']) || !arg.validate(['pass']))
-        throw Error('Not provide password or path to file');
-
-    const path = arg.get(['path']);
-    const pass = arg.get(['pass']);
-    const fileText = await fs.readFile(path, 'utf-8');
-
     try {
-        fs.writeFile(
-            path,
-            await decryptText(
-                JSON.parse(fileText).encrypted,
-                pass,
-                process.env.SALT,
-                JSON.parse(fileText).iv,
-            ),
-            'utf-8',
+        if (!arg.validate(['path']) || !arg.validate(['pass']))
+            throw Error('Not provide password or path to file');
+
+        const path = arg.get(['path']);
+        const pass = arg.get(['pass']);
+        const fileText = await fs.readFile(path, 'utf-8');
+        const fileTextObj = JSON.parse(fileText);
+        const decryptTextFile = await decryptText(
+            JSON.parse(fileText).encrypted,
+            pass,
+            process.env.SALT,
+            JSON.parse(fileText).iv,
         );
+
+        const decryptTextFileChecksum = createHmac('sha512', process.env.SALT)
+            .update(decryptTextFile)
+            .digest('hex');
+
+        if (fileTextObj.checksum !== decryptTextFileChecksum) throw Error('Wrong checksum.');
+
+        fs.writeFile(path, decryptTextFile, 'utf-8');
     } catch (e) {
         console.error(e);
     }
--- Level2/Week4/Day1/Encrypt/encrypt.js
+++ Level2/Week4/Day1/Encrypt/encrypt.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // import variables from .env file. .env file not send to Github
 const arg = require('../../../../helpers/arguments');
 const fs = require('fs').promises;
+const { createHmac } = require('crypto');
 const { encryptText } = require('../../../../helpers/cipher');
 
 (async () => {
@@ -11,11 +12,20 @@ const { encryptText } = require('../../../../helpers/cipher');
         const path = arg.get(['path']);
         const pass = arg.get(['pass']);
         const fileText = await fs.readFile(path, 'utf-8');
-        // const fileTextChecksum =
-        const encryptedText = await encryptText(fileText, pass, process.env.SALT);
-        const encryptedObj = JSON.stringify(encryptedText);
+        const fileTextChecksum = createHmac('sha512', process.env.SALT)
+            .update(fileText)
+            .digest('hex');
 
-        await fs.writeFile(path, encryptedObj).catch((err) => console.log(err));
+        const encryptedText = await encryptText(fileText, pass, process.env.SALT);
+
+        const encryptedObj = {
+            ...encryptedText,
+            ...{
+                checksum: fileTextChecksum,
+            },
+        };
+
+        await fs.writeFile(path, JSON.stringify(encryptedObj)).catch((err) => console.log(err));
     } catch (e) {
         console.error(e);
     }

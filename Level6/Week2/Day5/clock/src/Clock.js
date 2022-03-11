41786b7b27aa860c351d5ecc94a566b85db98a8c
wachcio <wachcio@poczta.fm>
2022-03-11 16:46:02 +0100

    React - Week2 - Day5 - clock

---
+++ Level6/Week2/Day5/clock/src/Clock.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+export class Clock extends Component {
+    state = {
+        time: new Date().toLocaleTimeString(),
+        date: new Date().toLocaleDateString(),
+    };
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState({
+                time: new Date().toLocaleTimeString(),
+                date: new Date().toLocaleDateString(),
+            });
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <h2>{this.state.time}</h2>
+                <h2>{this.state.date}</h2>
+            </div>
+        );
+    }
+}
--- Level6/Week2/Day5/task2/src/UserListCounti.js
+++
@@ -1,5 +0,0 @@
-function UserListCount(props) {
-    return <p>Wszystkich osób jest {props.userList.length}</p>;
-}
-
-export default UserListCount;

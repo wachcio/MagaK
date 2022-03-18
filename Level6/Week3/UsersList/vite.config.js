9487c9064b37bf31ae30f25463a9528d7a8e7a0e
wachcio <wachcio@poczta.fm>
2022-03-18 18:25:45 +0100

    change Vite config

--- Level6/Week3/UsersList/vite.config.js
+++ Level6/Week3/UsersList/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [
+        react(),
+        {
+            name: 'configure-response-headers',
+            configureServer: (server) => {
+                server.middlewares.use((_req, res, next) => {
+                    res.setHeader('Cross-Origin-Embedder-Policy', '*');
+                    res.setHeader('Cross-Origin-Opener-Policy', '*');
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    next();
+                });
+            },
+        },
+    ],
+    base: './',
+    server: {
+        cors: false,
+    },
+});

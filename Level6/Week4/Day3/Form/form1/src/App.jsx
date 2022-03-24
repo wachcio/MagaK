7d05d77e293b3bd40fb2182ba49df0f6372be452
wachcio <wachcio@poczta.fm>
2022-03-24 14:54:15 +0100

    select

--- Level6/Week4/Day3/Form/form1/src/App.jsx
+++ Level6/Week4/Day3/Form/form1/src/App.jsx
@@ -3,10 +3,34 @@ import './App.css';
 
 function App() {
     const [firstName, setFirstName] = useState('');
+    const [gender, setGender] = useState('M');
+
+    const color = firstName[0] === '@' ? 'green' : 'red';
+
+    const sendForm = (e) => {
+        e.preventDefault(0);
+        console.log(gender);
+    };
 
     return (
         <>
-            <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            <h1>Hello, {firstName}</h1>
+            <form
+                onSubmit={sendForm}
+                style={{ display: 'flex', flexDirection: 'column', width: '200px' }}
+            >
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    style={{ backgroundColor: color }}
+                />
+                <select name="gender" value={gender} onChange={(e) => setGender(e.target.value)}>
+                    <option value="">--wybierz--</option>
+                    <option value="W">Woman</option>
+                    <option value="M">Man</option>
+                </select>
+            </form>
         </>
     );
 }
